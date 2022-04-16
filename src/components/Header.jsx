d4bf9ba696510f6cfa0cb739314ad3db0463ed36
Vadzim Volkau <v.volkau@godeltech.com>
2022-04-16 21:39:46 +0300

    Add Header, Navbar and Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm67-Fqo9LyzxNT80mW33Y8BRTuVm3XdUYiw&usqp=CAU'/>
+        </header>
+    )
+}
+
+export default Header
