ca4512efeadb9f282034a261656a93a7202ad42d
Vadzim Volkau <v.volkau@godeltech.com>
2022-04-18 22:08:54 +0300

    Move data from attributes to array of objects

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,20 +19,35 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+    let dialogsData = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Andrew"},
+        {id: 3, name: "Sveta"},
+        {id: 4, name: "Sasha"},
+        {id: 5, name: "Viktor"},
+        {id: 6, name: "Valera"}
+    ]
+
+    let messagesData = [
+        {id: 1, message: "Hi!"},
+        {id: 2, message: "How are you"},
+        {id: 3, message: "Yo!"}
+    ]
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <DialogItem id="1" name="Dimych"/>
-                <DialogItem id="2" name="Andrey"/>
-                <DialogItem id="3" name="Sveta"/>
-                <DialogItem id="4" name="Sasha"/>
-                <DialogItem id="5" name="Victor"/>
-                <DialogItem id="6" name="Valera"/>
+                <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
+                <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}/>
+                <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}/>
+                <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}/>
+                <DialogItem id={dialogsData[4].id} name={dialogsData[4].name}/>
+                <DialogItem id={dialogsData[5].id} name={dialogsData[5].name}/>
             </div>
             <div className={style.messages}>
-                <Message message="Hi!"/>
-                <Message message="How are you?"/>
-                <Message message="Yo!"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import style from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let postData = [
+        {id: 1, message: "How are you?", likesCounter: 13},
+        {id: 2, message: "It's my first message", likesCounter: 23}
+    ]
+
     return (
         <div className={style.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +20,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={style.posts}>
-                <Post message="How are you?" likesCounter="0"/>
-                <Post message="It's my first message" likesCounter="23"/>
+                <Post message={postData[0].message} likesCounter={postData[0].likesCounter}/>
+                <Post message={postData[1].message} likesCounter={postData[1].likesCounter}/>
             </div>
         </div>
     )
