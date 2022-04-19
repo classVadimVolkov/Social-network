8e0fe9a462882ab5d14cbd9ef527553292f7c94d
Vadzim Volkau <v.volkau@godeltech.com>
2022-04-19 17:55:08 +0300

    Add DialogItem and Message components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,8 @@
 import React from "react";
 import style from "./Dialogs.module.css"
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id;
-
-    return (
-        <div className={style.dialog + ' ' + style.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={style.dialogs}>{props.message}</div>
-    )
-}
 
 const Dialogs = () => {
     let dialogs = [
