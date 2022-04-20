49b08cb79e0f7669f817c54a1d0515567bd8471c
Vadzim Volkau <v.volkau@godeltech.com>
2022-04-20 21:47:22 +0300

    Move fake data to state.js

--- src/index.js
+++ src/index.js
@@ -4,33 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import state from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-let dialogs = [
-    {id: 1, name: "Dimych"},
-    {id: 2, name: "Andrew"},
-    {id: 3, name: "Sveta"},
-    {id: 4, name: "Sasha"},
-    {id: 5, name: "Viktor"},
-    {id: 6, name: "Valera"}
-]
-
-let messages = [
-    {id: 1, message: "Hi!"},
-    {id: 2, message: "How are you"},
-    {id: 3, message: "Yo!"}
-]
-
-let posts = [
-    {id: 1, message: "How are you?", likesCounter: 13},
-    {id: 2, message: "It's my first message", likesCounter: 23}
-]
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App dialogs={dialogs} messages={messages} posts={posts}/>
+            <App state={state}/>
         </BrowserRouter>
     </React.StrictMode>
 );
