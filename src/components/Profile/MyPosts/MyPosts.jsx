88fb9c1ba0e7731ba7b75acde2f3aa7d96618163
Vadzim Volkau <v.volkau@godeltech.com>
2022-04-24 11:27:00 +0300

    Make "Add post" work

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,16 +4,21 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
     let postElements = props.posts.map(p => <Post message={p.message} likesCounter={p.likesCounter}/>)
+    let newPostElement = React.createRef()
+    let addPost = () => {
+        let text = newPostElement.current.value
+        props.addPost(text)
+    }
 
     return (
         <div className={style.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}/>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={ addPost }>Add post</button>
                 </div>
             </div>
             <div className={style.posts}>
