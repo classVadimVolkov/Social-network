7075d83c6b2d5a76765c4e57025541545e1a671f
Vadzim Volkau <v.volkau@godeltech.com>
2022-08-09 19:55:01 +0300

    Add useState() hook

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+const ProfileStatusWithHooks = (props) => {
+    let [editMode, setEditMode] = useState(false)
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+
+    let [status, setStatus] = useState(props.status)
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            {!editMode &&
+                <div>
+                    <span onDoubleClick={activateEditMode}>{props.status || 'My status'}</span>
+                </div>
+            }
+            {editMode &&
+                <div>
+                    <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode}
+                           value={status}/>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks
