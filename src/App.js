de4ff387879f30849dbe077ad233656cf63f58ae
Vadzim Volkau <v.volkau@godeltech.com>
2022-09-05 10:10:07 +0300

    Add React.Lazy and React.Suspense

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {Route, Routes} from 'react-router-dom';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {connect} from "react-redux";
 import {initializeApp} from "./redux/AppReducer";
 import Preloader from "./components/common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"))
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"))
+const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer"))
 
 class App extends React.Component {
     componentDidMount() {
@@ -26,13 +27,15 @@ class App extends React.Component {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path="/dialogs/*" element={<DialogsContainer/>}/>
-                        <Route path='/profile' element={<ProfileContainer/>}/>
-                        <Route path="/profile/:userId" element={<ProfileContainer store={this.props.store}/>}/>
-                        <Route path="/users" element={<UsersContainer/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
-                    </Routes>
+                    <React.Suspense fallback={<div><Preloader/></div>}>
+                        <Routes>
+                            <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                            <Route path='/profile' element={<ProfileContainer/>}/>
+                            <Route path="/profile/:userId" element={<ProfileContainer store={this.props.store}/>}/>
+                            <Route path="/users" element={<UsersContainer/>}/>
+                            <Route path="/login" element={<LoginPage/>}/>
+                        </Routes>
+                    </React.Suspense>
                 </div>
             </div>
         );
